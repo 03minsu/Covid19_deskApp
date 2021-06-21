ad7b1ff31c596f98b37cc2108a7ef4cee6f02edf
lyel10 <lyel10@naver.com>
2021-06-21 14:31:01 +0900

    0621

--- Covid19_DeskApp.py
+++ Covid19_DeskApp.py
@@ -7,20 +7,50 @@ import pymysql
 import sys
 
 from pyqtgraph.graphicsItems.ScatterPlotItem import Symbols
-
+# ㄴ 그래프 심볼 
+import test_rc
+# ㄴ 백그라운드 이미지 소스 파일
 
 conn = pymysql.connect(host='localhost', user='root', password='123456', db='covid19', charset='utf8')
 curs = conn.cursor()
 curs.execute("use covid19;")
+
 #UI파일 연결
 #단, UI파일은 Python 코드 파일과 같은 디렉토리에 위치해야한다.
 form_class = uic.loadUiType("Covid.ui")[0]
 main_class = uic.loadUiType("Main.ui")[0]
+menu_class = uic.loadUiType("Menu.ui")[0]
+# ㄴ 시작 화면 
 
-class WindowClass(QtWidgets.QMainWindow, form_class) :
+
+# 시작 화면 (초기 화면: 실행/종료)
+class MainClass(QtWidgets.QMainWindow, main_class):
+    def __init__(self) :    
+        super().__init__()
+        self.setupUi(self)
+                                                                           #  부모 클래스  메인창                #  자식 클래스 새창
+        self.pushButton.clicked.connect(self.menu)                        # def __init__(self) :    ->   def __init__(self, parent) :
+                                                                              # super().__init__()   ->      super([클래스명], self).__init__(parent)
+    def menu(self):                                                                                        # self.show()
+        MenuClass(self)                                                                     
+                                                                                                # 클래스 호출은 함수 안에 '[클래스명](self)' 로 선언
+# 메뉴 화면 (실행 버튼 클릭 시)
+class MenuClass(QtWidgets.QMainWindow, menu_class):
+    def __init__(self, parent) :
+        super(MenuClass, self).__init__(parent)
+        self.setupUi(self)
+        self.show()
+
+        self.MonthCovid.clicked.connect(self.covidGraph)
+
+    def covidGraph(self):
+        CovidClass(self)
+
+# 기능 페이지 / 코로나 월 별 확진자 (연도 별 그래프)
+class CovidClass(QtWidgets.QMainWindow, form_class) :
     
     def __init__(self, parent) :    
-        super(WindowClass, self).__init__(parent    )
+        super(CovidClass, self).__init__(parent)
         self.setupUi(self)
         self.btn3.clicked.connect(self.clear)
         self.btn1.clicked.connect(self.year2020)
@@ -59,16 +89,8 @@ class WindowClass(QtWidgets.QMainWindow, form_class) :
             y.append(row[1])
         self.plot(x,y)
 
-class MainClass(QtWidgets.QMainWindow, main_class):
-    def __init__(self) :    
-        super().__init__()
-        self.setupUi(self)
-        
-        self.pushButton.clicked.connect(self.covid)
-    
-    def covid(self):
-        WindowClass(self)
 
+# 최초 실행 로직 --
 def main():
     app = QApplication(sys.argv) 
     main = MainClass() 
